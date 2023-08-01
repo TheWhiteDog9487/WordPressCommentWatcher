a1d70a710055de887ea8bd8f219462a8e533de72
TheWhiteDog9487 <60037547+TheWhiteDog9487@users.noreply.github.com>
2023-08-01 11:50:48 +0000

    结构修改，增加了新功能。 我就不相信用上了配置文件之后我令牌还能掉。

--- README.md
+++ README.md
@@ -3,7 +3,23 @@
 
 # 部署
 从仓库下载"WordPress评论监控.py"，打开。  
-看一下里边的注释，有两个地方需要换成你自己的信息。  
+首次运行会直接退出，目录下多出来一个json配置文件，打开。  
+Bot_Token：机器人的令牌  
+Channel_Message：如果你希望机器人在一个服务器的频道内提醒你，这里就填true；如果你希望机器人私信你，那就填false。  
+Admin_User_ID：选择私信提醒的朋友，这里填上你的Discord用户ID，选择频道提醒的不用理会。  
+Channel_ID：选择频道提醒的朋友，这里填上你想让机器人发送消息的频道ID，选择私信提醒的不用理会。  
+注意：Admin_User_ID和Channel_ID直接写数字就行，不用像上面的Token那样加引号。Channel_Message直接填true或false，也不要加引号。  
+    
+填写完成之后应该大致长这样：
+```json
+{
+	"Bot_Token":"MTA4ODA2OTI5Nzg2MTA1MDQ0OA.GZkIBA.-11qdvzOz5o3zFiC-pW1YjbpFOaa93Q1vycOYg",
+	"Admin_User_ID":785275297272909078,
+	"Channel_ID":4987138441723578956,
+	"Channel_Message":true
+}
+```
+( 献祭一下以前的令牌，来这当下例子
 
 # 依赖项
 需要pip里的discord.py包。
--- WordPress评论监控.py
+++ WordPress评论监控.py
@@ -1,31 +1,33 @@
 import discord
 from datetime import datetime
-from datetime import timezone
 import logging
 import requests
 import os
 import time
 import platform
 import zipfile
+import json
 
 intents = discord.Intents.default()
 intents.messages = True
 BotClient = discord.Client(intents=intents)
-NewComment = ""
-
+Config = {}
+Message = ""
 
 @BotClient.event
 async def on_ready():
-    print(f'以{BotClient.user}登录')
-    user = await BotClient.fetch_user(你的用户ID)
-    # 记得把你的ID换上去
-    # 开启Discord的开发者模式，返回主界面，单击左下角你的头像，点击复制用户ID
-    # 直接粘贴到上面，不用加引号，直接传数字就可以
-    await user.send(NewComment)
+    logging.info(f'以{BotClient.user}登录')
+    if Config["Channel_Message"]:
+        channel = BotClient.get_channel(Config["Channel_ID"])
+        await channel.send(f"<@{Config['Admin_User_ID']}>\n{Message}")
+    else:
+        user = await BotClient.fetch_user(Config["Admin_User_ID"])
+        await user.send(Message)
+    logging.info('已发送新通知\n')
     await BotClient.close()
 
 
-def TimerTrigger() -> None:
+def init():
     LogPath = ""
     if platform.system() == 'Windows':
         LogPath = os.curdir + "\\日志\\"
@@ -34,49 +36,64 @@ def TimerTrigger() -> None:
     LogFileName = LogPath + "WordPress评论监控.log"
     if not os.path.exists(LogPath):
         os.makedirs(LogPath)
-    if not os.path.exists('UpdateTime.txt'):
-        with open('UpdateTime.txt', 'w', encoding="utf-8") as w:
-            w.write("")
-
-
-
+    if not os.path.exists('最新评论发布时间.txt'):
+        with open('最新评论发布时间.txt', 'w', encoding="utf-8") as w:
+            w.write(datetime.strptime(
+                requests.get('https://www.thewhitedog9487.xyz/wp-json/wp/v2/comments').json()[0]["date"],
+                '%Y-%m-%dT%H:%M:%S').strftime('%Y年%m月%d日 %H时%M分%S秒'))
     if os.path.exists(LogFileName):
         if os.path.getsize(LogFileName) > 1024 * 1024 * 10:
             with zipfile.ZipFile(LogFileName, 'w', zipfile.ZIP_DEFLATED) as zf:
                 zf.write(datetime.now().strftime('%Y%m%d') + '.zip')
             os.remove(LogFileName)
-    
-
-
     logging.basicConfig(filename=LogFileName, level=logging.INFO, encoding="utf-8")
     logging.info(f'程序在{datetime.now().replace().strftime("%Y-%m-%d %H:%M:%S")}运行')
+    if not os.path.exists("配置文件.json"):
+        with open("配置文件.json", "w", encoding="utf-8") as w:
+            w.write("{\n\t\"Bot_Token\":\"\",\n\t\"Admin_User_ID\":1234,\n\t\"Channel_ID\":5678,\n\t\"Channel_Message\":true\n}")
+            logging.info('首次运行')
+            logging.error('配置文件模板已生成，请填写配置文件')
+        exit()
+    with open("配置文件.json", "r", encoding="utf-8") as r:
+        global Config
+        Config = json.load(r)
+    if Config["Bot_Token"] == "":
+        logging.error('请填写机器人令牌')
+        logging.error('这是必填项，如果为空程序将不能运行')
+        exit()
+    if Config["Channel_Message"]:
+        logging.debug('程序在频道中运行')
+        if Config["Channel_ID"] == "":
+            logging.error('请填写频道ID')
+            logging.error('这是必填项，如果为空程序将不能运行')
+            exit()
+    else:
+        logging.debug('程序在私信中运行')
+        if Config["Admin_User_ID"] == "":
+            logging.error('请填写Discord管理员用户ID')
+            logging.error('这是必填项，如果为空程序将不能运行')
+            exit()
+
+def TimerTrigger() -> None:
     for comment in requests.get('https://www.thewhitedog9487.xyz/wp-json/wp/v2/comments').json():
         RemoteCommentDate = int(time.mktime(time.strptime(comment["date"], '%Y-%m-%dT%H:%M:%S')))
-        with open('UpdateTime.txt', 'r', encoding="utf-8") as r:
+        with open('最新评论发布时间.txt', 'r', encoding="utf-8") as r:
             FileCommentDate = r.readline()
-            if FileCommentDate == "":
-                with open('UpdateTime.txt', 'w', encoding="utf-8") as w:
-                    w.write(datetime.strptime(comment["date"], '%Y-%m-%dT%H:%M:%S').strftime('%Y-%m-%d %H:%M:%S'))
-                logging.info('首次运行')
-                break
-            FileCommentDate = int(time.mktime(time.strptime(FileCommentDate, '%Y-%m-%d %H:%M:%S')))
+            FileCommentDate = int(time.mktime(time.strptime(FileCommentDate, '%Y年%m月%d日 %H时%M分%S秒')))
             if int(FileCommentDate) < RemoteCommentDate:
-                global NewComment
                 logging.info('有新评论')
-                with open('UpdateTime.txt', 'w', encoding="utf-8") as w:
-                    w.write(str(RemoteCommentDate))
-                if FileCommentDate != "":
-                    NewComment = f'评论者：{comment["author_name"]}\n评论内容：{comment["content"]["rendered"]}评论链接：{comment["link"]}\n评论时间：{comment["date"]}'
-                    BotClient.run('你的机器人令牌')
-                    # 记得填！
-                    # 你自己的令牌！
-                    logging.info('已发送新通知\n')
+                with open('最新评论发布时间.txt', 'w', encoding="utf-8") as w:
+                    w.write(time.strftime('%Y年%m月%d日 %H时%M分%S秒', time.localtime(RemoteCommentDate)))
+                    global Message
+                    Message = f'评论者：{comment["author_name"]}\n评论内容：{comment["content"]["rendered"]}评论链接：{comment["link"]}\n评论时间：{comment["date"]}'
+                    BotClient.run(Config["Bot_Token"])
             elif int(FileCommentDate) > RemoteCommentDate:
                 logging.info('有评论被删除\n')
-            else:
+            elif int(FileCommentDate) == RemoteCommentDate:
                 logging.info('没有新评论\n')
         break
 
 
 if __name__ == '__main__':
+    init()
     TimerTrigger()
