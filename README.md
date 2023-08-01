9428c021bc795f3c694de0607a39277db1ff0927
TheWhiteDog9487 <60037547+TheWhiteDog9487@users.noreply.github.com>
2023-08-01 09:16:55 +0000

    几个小细节优化，下一步就要改结构了。

--- README.md
+++ README.md
@@ -2,10 +2,8 @@
 关于这个东西可以看[这篇文章](https://www.thewhitedog9487.xyz/2023/07/31/%e8%bf%91%e4%ba%8b%e5%b0%8f%e8%ae%b0-%e5%8d%9a%e5%ae%a2%e5%8f%91%e7%9a%84%e8%af%84%e8%ae%ba%e5%8f%af%e4%bb%a5%e5%8f%8a%e6%97%b6%e9%80%9a%e7%9f%a5%e5%88%b0%e6%88%91%e4%ba%86)
 
 # 部署
-注意，这个程序是对Linux定制的（主要是文件路径那边的问题）  
-如果你要在Windows下用，要修改一下。  
-从仓库下载"WordPress评论监控.py"，打开，看一下里边的注释，有两个地方需要换成你自己的信息。  
-另外，记得运行之前在脚本所在目录手动建一个UpdateTime.txt  
+从仓库下载"WordPress评论监控.py"，打开。  
+看一下里边的注释，有两个地方需要换成你自己的信息。  
 
 # 依赖项
 需要pip里的discord.py包。
@@ -17,10 +15,15 @@ pip install discord.py
 apt install python3-discord
 ```
 
-# 特别说明
+# 定时器
+本程序不负责定时调用，所以需要你来处理。  
+Linux用户可以用cron，Windows我没具体试过所以自己查一下咋整。  
+## 注意事项
 如果你要用cron做定时器，注意会有一些问题。  
 如果你直接在cron里写  
-\* \* \* \* \* python3 WordPress评论监控.py  
+```cron
+* * * * * python3 WordPress评论监控.py
+```
 你会发现日志里有点奇怪，而且完全没有实际功能。  
 解决方案是，建一个Shell脚本，比如叫做WordPress评论监控.sh，里面的内容可以这么写：
 ```bash
@@ -29,5 +32,7 @@ cd /home/App/
 /usr/bin/python3 WordPress评论监控.py
 ```
 crontab里换成这样：  
-\* \* \* \* \* python3 WordPress评论监控.sh  
+```cron
+* * * * * python3 WordPress评论监控.sh
+```
 小提示：5个\*表示每分钟调用一次。  
--- WordPress评论监控.py
+++ WordPress评论监控.py
@@ -1,46 +1,77 @@
 import discord
-import datetime
+from datetime import datetime
+from datetime import timezone
 import logging
 import requests
 import os
+import time
+import platform
+import zipfile
 
 intents = discord.Intents.default()
 intents.messages = True
 BotClient = discord.Client(intents=intents)
 NewComment = ""
 
+
 @BotClient.event
 async def on_ready():
     print(f'以{BotClient.user}登录')
-    user = await BotClient.fetch_user(你的用户ID)
-    # 记得把你的ID换上去
-    # 开启Discord的开发者模式，返回主界面，单击左下角你的头像，点击复制用户ID
-    # 直接粘贴到上面，不用加引号，直接传数字就可以
+    user = await BotClient.fetch_user(531317938863603722)
     await user.send(NewComment)
     await BotClient.close()
 
+
 def TimerTrigger() -> None:
-    LogPath = "/var/log/thewhitedog9487/"
-    if os.path.exists(LogPath) == False:
+    LogPath = ""
+    if platform.system() == 'Windows':
+        LogPath = os.curdir + "\\日志\\"
+    elif platform.system() == 'Linux':
+        LogPath = "/var/log/thewhitedog9487/"
+    LogFileName = LogPath + "WordPress评论监控.log"
+    if not os.path.exists(LogPath):
         os.makedirs(LogPath)
-    logging.basicConfig(filename=f'{LogPath}WordPress评论监控.log', level=logging.INFO)
-    logging.info(f'程序在{datetime.datetime.utcnow().replace(tzinfo=datetime.timezone.utc).isoformat()}运行')
+    if not os.path.exists('UpdateTime.txt'):
+        with open('UpdateTime.txt', 'w', encoding="utf-8") as w:
+            w.write("")
+
+
+
+    if os.path.exists(LogFileName):
+        if os.path.getsize(LogFileName) > 1024 * 1024 * 10:
+            with zipfile.ZipFile(LogFileName, 'w', zipfile.ZIP_DEFLATED) as zf:
+                zf.write(datetime.now().strftime('%Y%m%d') + '.zip')
+            os.remove(LogFileName)
+    
+
+
+    logging.basicConfig(filename=LogFileName, level=logging.INFO, encoding="utf-8")
+    logging.info(f'程序在{datetime.now().replace().strftime("%Y-%m-%d %H:%M:%S")}运行')
     for comment in requests.get('https://www.thewhitedog9487.xyz/wp-json/wp/v2/comments').json():
-        with open('UpdateTime.txt', 'r') as r:
-            Line = r.readline()
-            if(Line != comment["date"]):
+        RemoteCommentDate = int(time.mktime(time.strptime(comment["date"], '%Y-%m-%dT%H:%M:%S')))
+        with open('UpdateTime.txt', 'r', encoding="utf-8") as r:
+            FileCommentDate = r.readline()
+            if FileCommentDate == "":
+                with open('UpdateTime.txt', 'w', encoding="utf-8") as w:
+                    w.write(datetime.strptime(comment["date"], '%Y-%m-%dT%H:%M:%S').strftime('%Y-%m-%d %H:%M:%S'))
+                logging.info('首次运行')
+                break
+            FileCommentDate = int(time.mktime(time.strptime(FileCommentDate, '%Y-%m-%d %H:%M:%S')))
+            if int(FileCommentDate) < RemoteCommentDate:
                 global NewComment
                 logging.info('有新评论')
-                with open('UpdateTime.txt', 'w') as w:
-                    w.write(comment["date"])
-                if Line != "":
-                    NewComment = f'评论者：{comment["author_name"]}\n评论内容：{comment["content"]["rendered"]}\n评论链接：{comment["link"]}\n评论时间：{comment["date"]}'
-                    BotClient.run("你的机器人令牌")
-                    # 记得填！
-                    # 你自己的令牌！
+                with open('UpdateTime.txt', 'w', encoding="utf-8") as w:
+                    w.write(str(RemoteCommentDate))
+                if FileCommentDate != "":
+                    NewComment = f'评论者：{comment["author_name"]}\n评论内容：{comment["content"]["rendered"]}评论链接：{comment["link"]}\n评论时间：{comment["date"]}'
+                    BotClient.run('MTA4ODA2OTI5Nzg2MTA1MDQ0OA.GZkIBA.-11qdvzOz5o3zFiC-pW1YjbpFOaa93Q1vycOYg')
                     logging.info('已发送新通知\n')
+            elif int(FileCommentDate) > RemoteCommentDate:
+                logging.info('有评论被删除\n')
             else:
                 logging.info('没有新评论\n')
         break
 
-TimerTrigger()
+
+if __name__ == '__main__':
+    TimerTrigger()
